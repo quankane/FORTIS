69b79559e7784af7cdaba244808f318a4df59279
quankane <quanducbui2017@gmail.com>
2025-09-08 23:46:49 +0700

    Initial base

---
+++ src/main/java/vn/com/fortis/FortisApplication.java
@@ -0,0 +1,13 @@
+package vn.com.fortis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortisApplication.class, args);
+	}
+
+}
