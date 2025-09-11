713a97abe97b40a12ecf4a70cbdc65e8673791b9
quankane <quanducbui2017@gmail.com>
2025-09-11 22:00:44 +0700

    Fix: Fix config error on dialect of Database

---
+++ src/main/java/vn/com/fortis/config/DotenvInitializer.java
@@ -0,0 +1,20 @@
+package vn.com.fortis.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        //Load env
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        // Add all vars to system properties
+        dotenv.entries().forEach(entry
+                -> System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
+}
--- src/test/java/vn/com/fortis/FortisApplicationTests.java
+++ src/test/java/vn/com/fortis/FortisApplicationTests.java
@@ -2,12 +2,16 @@ package vn.com.fortis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import vn.com.fortis.config.DotenvInitializer;
 
 @SpringBootTest
+@ContextConfiguration(initializers = DotenvInitializer.class)
 class FortisApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//
 	}
 
 }
