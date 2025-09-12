acc51d75c2eead4d5cb2cde8d3bd261808970d28
quankane <quanducbui2017@gmail.com>
2025-09-12 10:23:59 +0700

    Completely Customize pre filter for security

--- src/main/java/vn/com/fortis/security/CustomizePreFilter.java
+++ src/main/java/vn/com/fortis/security/CustomizePreFilter.java
@@ -7,23 +7,91 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import vn.com.fortis.constant.TokenType;
+import vn.com.fortis.service.JwtService;
 
 import java.awt.*;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static vn.com.fortis.constant.TokenType.ACCESS_TOKEN;
 
 @Component
 @RequiredArgsConstructor
 @EnableMethodSecurity
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j(topic = "CUSTOMIZE-PRE-FILTER")
 public class CustomizePreFilter extends OncePerRequestFilter {
 
+    CustomUserDetailsService customUserDetailsService;
 
+    JwtService jwtService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("{} {}", request.getMethod(), request.getRequestURI());
 
+        final String authHeader = request.getHeader("Authorization");
+
+        if (StringUtils.isBlank(authHeader) || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String token = authHeader.substring(7);
+
+        try {
+            String username = jwtService.extractUserName(token, ACCESS_TOKEN);
+
+            if (StringUtils.isNoneBlank(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+                if (jwtService.isValid(token, ACCESS_TOKEN, userDetails.getUsername())) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                    securityContext.setAuthentication(authenticationToken);
+                    SecurityContextHolder.setContext(securityContext);
+                }
+            }
+        } catch (Exception ex) {
+            log.error("Invalid token = {}, message = {}", ex.getClass(), ex.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            String json = buildErrorJson(HttpServletResponse.SC_UNAUTHORIZED, request.getRequestURI(), "Unauthorized", "Invalid or missing JWT Token");
+            response.getWriter().write(json);
+        }
+    }
+
+    private String buildErrorJson(int status, String path, String error, String message) {
+        return String.format("""
+        {
+            "timestamp": "%s",
+            "status": %d,
+            "path": "%s",
+            "error": "%s",
+            "message": "%s",
+        }
+        """,
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("MMM d, yyyy, h:mm:ss a", Locale.ENGLISH)),
+                status,
+                path,
+                error,
+                message
+        );
     }
 }
