a7faf1973ffe58313adeebb13df1b3511236ff95
quankane <quanducbui2017@gmail.com>
2025-10-23 16:55:06 +0700

    Update vnpay config

--- src/main/java/vn/com/fortis/config/VNPAYConfig.java
+++ src/main/java/vn/com/fortis/config/VNPAYConfig.java
@@ -10,52 +10,46 @@ import java.util.Map;
 @Configuration
 @Getter
 public class VNPAYConfig {
-    //VNPAY_URL=https://sandbox.vnpayment.vn/paymentv2/vpcpay.html
-    //VNPAY_RETURN_URL=https://socko-stratagemical-abdullah.ngrok-free.dev/api/v1/payment/vnpay-return
-    //VNPAY_TMN_CODE=8X9ZJV1I #VNP CUNG CẤP
-    //VNPAY_HASH_SECRET=5XB2H37CCYP3UEC4DNAPO8P7WA62G148 #VNP CUNG CẤP
-    //VNPAY_VERSION=2.1.0
-    //VNPAY_COMMAND=pay
-    //VNPAY_MAXTIME=900
-    //VNPAY_ORDER_TYPE=other
-    //VNPAY_CURRCODE=VND
-    //VNPAY_LOCALE=vn
-    @Value("${payment.vnpayUrl}")
-    private String vnp_Url;
 
-    @Value("${payment.vnpayReturnUrl}")
+    @Value("${payment.vnPay.url}")
+    private String vnp_PayUrl;
+
+    @Value("${payment.vnPay.returnUrl}")
     private String vnp_ReturnUrl;
 
-    @Value("${payment.vnpayTmnCode}")
+    @Value("${payment.vnPay.tmnCode}")
     private String vnp_TmnCode;
 
-    @Value("${payment.vnpayHashSecret}")
+    @Value("${payment.vnPay.hashSecret}")
     private String vnp_HashSecret;
 
-    @Value("${payment.vnpVersion}")
+    @Value("${payment.vnPay.version}")
     private String vnp_Version;
 
-    @Value("${payment.vnpayCommand}")
+    @Value("${payment.vnPay.command:pay}")
     private String vnp_Command;
 
-    @Value("${payment.vnpayOrderType}")
+    @Value("${payment.vnPay.orderType}")
     private String vnp_OrderType;
 
-    @Value("${payment.vnpayCurrCode}")
+    @Value("${payment.vnPay.currCode:VND}")
     private String vnp_CurrCode;
 
-    @Value("${payment.vnpayLocale}")
+    @Value("${payment.vnPay.locale:vn}")
     private String vnp_Locale;
 
+
+
     public Map<String, String> getConfig() {
-        Map<String, String> params = new HashMap<>();
-        params.put("vnp_Command", vnp_Command);
-        params.put("vnp_CurrCode", vnp_CurrCode);
-        params.put("vnp_Locale", vnp_Locale);
-        params.put("vnp_OrderType", vnp_OrderType);
-        params.put("vnp_ReturnUrl", vnp_ReturnUrl);
-        params.put("vnp_TmnCode", vnp_TmnCode);
-        params.put("vnp_Version", vnp_Version);
-        return params;
+        Map<String, String> vnpParamsMap = new HashMap<>();
+        vnpParamsMap.put("vnp_Version", this.vnp_Version);
+        vnpParamsMap.put("vnp_Command", this.vnp_Command);
+        vnpParamsMap.put("vnp_TmnCode", this.vnp_TmnCode);
+        vnpParamsMap.put("vnp_CurrCode", this.vnp_CurrCode);
+        vnpParamsMap.put("vnp_Locale", this.vnp_Locale);
+        vnpParamsMap.put("vnp_ReturnUrl", this.vnp_ReturnUrl);
+        vnpParamsMap.put("vnp_OrderType", this.vnp_OrderType);
+        return vnpParamsMap;
     }
+
 }
